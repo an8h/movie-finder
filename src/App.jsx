f876ae054df5f889f2120e0873b22aa53c639edf
an8h <anthie10@hotmail.com>
2020-05-19 20:07:39 +0200

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// const App = ({ title }) => <div>{title}</div>;
+const App = () => <div>Find some movies</div>;
+
+export default App;
---
+++ src/App.spec.js
@@ -0,0 +1,5 @@
+describe('Example Test', () => {
+  it('Does not do much', () => {
+    expect(true).toEqual(true);
+  });
+});
