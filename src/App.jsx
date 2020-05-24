0708b2b48a8b31f64b0bae888a1104c1de52b0a2
an8h <anthie10@hotmail.com>
2020-05-24 08:53:31 +0200

    restructure App component and add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+/* eslint-disable react/jsx-fragments */
+import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
+import Store from './redux/Store';
+import './App.css';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
-// const App = ({ title }) => <div>{title}</div>;
-const App = () => <div>Find some movies</div>;
+const App = () => {
+  return (
+    <Provider store={Store}>
+      <Fragment>
+        <Home />
+        <Footer />
+      </Fragment>
+    </Provider>
+  );
+};
 
 export default App;
