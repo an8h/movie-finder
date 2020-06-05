263b7c2ffa46e8fb1baa8f1cd862764b7c84162e
an8h <anthie10@hotmail.com>
2020-06-05 05:49:46 +0200

    add pageNotFound component

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home/index';
 import DetailPage from '../components/DetailPage/index';
 import WatchVideo from '../components/WatchVideo/index';
+import PageNotFound from '../components/PageNotFound/index';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/detail" component={DetailPage} />
     <Route path="/watchVideo" component={WatchVideo} />
+    <Route component={PageNotFound} />
   </Switch>
 );
 
