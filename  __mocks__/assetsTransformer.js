5332a84de2e80c45f0617648c7b422ab018979bd
an8h <anthie10@hotmail.com>
2020-06-06 17:07:51 +0200

    jest configuration

---
+++  __mocks__/assetsTransformer.js
@@ -0,0 +1,12 @@
+/* eslint-disable prefer-template */
+const path = require('path');
+
+module.exports = {
+  process(src, filename) {
+    return (
+      'module.exports = ' +
+      JSON.stringify(path.basename(filename)) +
+      ';'
+    );
+  },
+};
